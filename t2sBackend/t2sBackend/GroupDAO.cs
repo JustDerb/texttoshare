48f6cd5befc9e37426c4f97cf5e7dc0cc80bf147
Nicole Bruck <nicole.a.bruck@gmail.com>
2013-02-27 20:44:10 -0600

    Implemented methods in GroupDAO

--- t2sBackend/t2sBackend/GroupDAO.cs
+++ t2sBackend/t2sBackend/GroupDAO.cs
@@ -7,122 +7,166 @@ namespace t2sBackend
 {
     public class GroupDAO
     {
+        /// <summary>
+        /// The List of Users in the Group. Managed by the Group Moderators
+        /// </summary>
         public List<UserDAO> Users
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            get;
+            set;
         }
 
+        /// <summary>
+        /// Used to manage the Group in the Database
+        /// </summary>
         public int GroupID
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            get;
+            set;
         }
 
+        /// <summary>
+        /// The name of the Group
+        /// </summary>
         public string Name
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            get;
+            set;
         }
 
+        /// <summary>
+        /// The description to be displayed about the Group
+        /// </summary>
         public string Description
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            get;
+            set;
         }
 
+        /// <summary>
+        /// The List of Users that will have access to the Group's Users and PlugIns
+        /// </summary>
         public List<UserDAO> Moderators
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            get;
+            set;
         }
 
+        /// <summary>
+        /// The creator of the Group
+        /// </summary>
         public UserDAO Owner
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            get;
+            private set;
         }
 
+        /// <summary>
+        /// The List of Plugins that the Group's Moderators have chosen to enable for the Group
+        /// </summary>
         public List<IPlugin> EnabledPlugins
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            get;
+            set;
         }
 
+        /// <summary>
+        /// The Tag for the Group that will be used to identify in command messages
+        /// </summary>
         public string GroupTag
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            get;
+            set;
         }
 
+        /// <summary>
+        /// Adds a User to the Group
+        /// </summary>
+        /// <param name="user">User to be added</param>
+        /// <returns>True if User was added successfully, False if otherwise</returns>
         public bool AddUserToGroup(UserDAO user)
         {
-            throw new System.NotImplementedException();
+            if (user == null)
+            {
+                return false;
+            }
+            Users.Add(user);
+            return true;
         }
 
+        /// <summary>
+        /// Removes a User from the Group
+        /// </summary>
+        /// <param name="user">User to be removed</param>
+        /// <returns>True if User was removed successfully, false if otherwise</returns>
         public bool RemoveUserFromGroup(UserDAO user)
         {
-            throw new System.NotImplementedException();
+            if (!Users.Contains(user))
+            {
+                return false;
+            }
+            Users.Remove(user);
+            return true;
         }
 
+        /// <summary>
+        /// Adds a Moderator to the Group
+        /// </summary>
+        /// <param name="user">User to be added as a Moderator</param>
+        /// <returns>True if User is added as a Moderator successfully, false if otherwise</returns>
         public bool AddModerator(UserDAO user)
         {
-            throw new System.NotImplementedException();
+            if (!Users.Contains(user) || Moderators.Contains(user))
+            {
+                return false;
+            }
+            Moderators.Add(user);
+            return true;
         }
 
+        /// <summary>
+        /// Removes a User as a Moderator for the Group
+        /// </summary>
+        /// <param name="user">User to be removed from Moderators</param>
+        /// <returns>True if User is removed successfully, false if otherwise</returns>
         public bool RemoveModerator(UserDAO user)
         {
-            throw new System.NotImplementedException();
+            if (!Moderators.Contains(user))
+            {
+                return false;
+            }
+            Moderators.Remove(user);
+            return true;
         }
 
+        /// <summary>
+        /// Adds a Plugin to the Groups List of enabled Plugins
+        /// </summary>
+        /// <param name="plugin">Plugin to be added</param>
+        /// <returns>True if the Plugin is added successfully, false if otherwise</returns>
         public bool AddPlugin(IPlugin plugin)
         {
-            throw new System.NotImplementedException();
+            if (EnabledPlugins.Contains(plugin))
+            {
+                return false;
+            }
+            EnabledPlugins.Add(plugin);
+            return true;
         }
 
+        /// <summary>
+        /// Removes a Plugin from the Group's List of enabled Plugins
+        /// </summary>
+        /// <param name="plugin">Plugin to be removed</param>
+        /// <returns>True if the Plugin is removed successfully, false if otherwise</returns>
         public bool RemovePlugin(IPlugin plugin)
         {
-            throw new System.NotImplementedException();
+            if (!EnabledPlugins.Contains(plugin))
+            {
+                return false;
+            }
+            EnabledPlugins.Remove(plugin);
+            return true;
         }
     }
 }
