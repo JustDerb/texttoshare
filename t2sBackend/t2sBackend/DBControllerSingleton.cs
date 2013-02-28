8fb972983e682ed18c7e0ef8531a430a895cb43d
staley7 <staleylance@gmail.com>
2013-02-27 20:06:07 -0600

    Created DBControllerSingleton

---
+++ t2sBackend/t2sBackend/DBControllerSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace t2sBackend
+{
+    public static class DBControllerSingleton{
+    
+
+        private static IDBController _Controller;
+
+        private static IDBController Controller
+        {
+            get
+            {
+                if (_Controller == null)
+                {
+                    _Controller = new SQLController();
+                }
+                return _Controller;
+            }
+            set
+            {
+                _Controller = value;
+            }
+        }
+
+        public static IDBController getInstance()
+        {
+            return Controller;
+        }
+    }
+}
