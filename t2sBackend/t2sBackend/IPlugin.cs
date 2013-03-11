3396fb14d619e52819cad7293735d5496416fd6e
Nicole Bruck <nicole.a.bruck@gmail.com>
2013-03-11 17:45:19 -0500

    Documentation of IPlugin
    Assuming this class is going to be changed quite a bit...

--- t2sBackend/t2sBackend/IPlugin.cs
+++ t2sBackend/t2sBackend/IPlugin.cs
@@ -5,39 +5,61 @@ using System.Text;
 
 namespace t2sBackend
 {
+    /// <summary>
+    /// Backbone for all plug-ins to be used
+    /// </summary>
     public interface IPlugin
     {
-
+        /// <summary>
+        /// Controls what the plug-in has access to in the system
+        /// </summary>
         t2sBackend.PluginAccess Access
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Command that identifies plug-in
+        /// </summary>
         string Command
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Description of plug-in
+        /// </summary>
         string HelpText
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Whether the plug-in will be available to the users
+        /// </summary>
         bool IsDisabled
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Whether the plug-in will be displayed to the users
+        /// </summary>
         bool IsHidden
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Intializes the plug-in thread
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="service"></param>
         void Run(ParsedMessage message, AWatcherService service);
     }
 }
