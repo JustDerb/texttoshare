9cf5260a335eb5d645015f9397154624191c1031
Nicole Bruck <nicole.a.bruck@gmail.com>
2013-03-11 21:07:55 -0500

    Revert "Revert "First draft of PluginError""
    This reverts commit 054ece48a7c955b6aceceafaadb597467a98ecfc.

--- t2sBackend/t2sBackend/PluginError.cs
+++ t2sBackend/t2sBackend/PluginError.cs
@@ -5,71 +5,79 @@ using System.Text;
 
 namespace t2sBackend
 {
+    /// <summary>
+    /// Class which handles errors with plug-ins
+    /// </summary>
     public class PluginError : IPlugin
     {
+        /// <summary>
+        /// Controls access to the system
+        /// </summary>
         public PluginAccess Access
         {
             get
             {
-                throw new NotImplementedException();
+                return PluginAccess.MODERATOR;
             }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            private set;
         }
 
+        /// <summary>
+        /// Returns command to call Error plug-in
+        /// </summary>
         public string Command
         {
             get
             {
-                throw new NotImplementedException();
+                return "ERROR";
             }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            private set;
         }
 
+        /// <summary>
+        /// Returns information about the Error plug-in
+        /// </summary>
         public string HelpText
         {
             get
             {
-                throw new NotImplementedException();
+                return "Notifies User with a specified detail message";
             }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            private set;
         }
 
+        /// <summary>
+        /// Returns that Error plug-in is not disabled
+        /// </summary>
         public bool IsDisabled
         {
             get
             {
-                throw new NotImplementedException();
+                return false;
             }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            private set;
         }
 
+        /// <summary>
+        /// Returns that the Error plug-in is not hidden
+        /// </summary>
         public bool IsHidden
         {
             get
             {
-                throw new NotImplementedException();
+                return false;
             }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            private set;
         }
 
+        /// <summary>
+        /// Takes in the ParsedMessage and sends a message back to the sender with a specified detailed message about the error
+        /// </summary>
+        /// <param name="message">Message in error to be handled</param>
+        /// <param name="service">Service to send/recieve messages through</param>
         public void Run(ParsedMessage message, AWatcherService service)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
