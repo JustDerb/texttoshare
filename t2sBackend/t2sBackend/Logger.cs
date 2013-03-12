ca8440b86fb544200135861d6e4ce2abf25fedbd
staley7 <staleylance@gmail.com>
2013-03-11 21:09:33 -0500

    logger changed to static

--- t2sBackend/t2sBackend/Logger.cs
+++ t2sBackend/t2sBackend/Logger.cs
@@ -6,11 +6,25 @@ using System.Text;
 
 namespace t2sBackend
 {
-    public class Logger
+
+    /// <summary>
+    /// static class which logs messages to the database
+    /// </summary>
+    public static class Logger
     {
-        public bool LogMessage(string message, LoggerLevel level)
+        /// <summary>
+        /// private static IDBController variable
+        /// </summary>
+        private static IDBController controller = new SQLController();
+        /// <summary>
+        /// takes the IDBController instance and logs a message with the LoggerLevel
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        public static bool LogMessage(string message, LoggerLevel level)
         {
-            return new SQLController().LogMessage(message, level);
+            return controller.LogMessage(message, level);
         }
     }
 }
