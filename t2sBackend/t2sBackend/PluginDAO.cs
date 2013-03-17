04b45a934fa4999202f4abe1014dc26cfa936590
Cameron Legleiter <camlegleiter@gmail.com>
2013-03-17 18:29:25 -0500

    Added new PluginDAO to interface with the database

---
+++ t2sBackend/t2sBackend/PluginDAO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t2sBackend
+{
+    public class PluginDAO
+    {
+        public int PluginID
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public bool isDisabled
+        {
+            get;
+            set;
+        }
+
+        public string VersionNum
+        {
+            get;
+            set;
+        }
+
+        public int OwnerID
+        {
+            get;
+            set;
+        }
+
+        private UserDAO Owner
+        {
+            get;
+            set;
+        }
+    }
+}
