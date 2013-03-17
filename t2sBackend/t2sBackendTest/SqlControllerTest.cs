b19418d8a9990a99f699bff03457542ecc63b9eb
Cameron Legleiter <camlegleiter@gmail.com>
2013-03-17 00:17:42 -0500

    Added new test file for SqlController class

---
+++ t2sBackend/t2sBackendTest/SqlControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace t2sBackendTest
+{
+    [TestClass]
+    public class SqlControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
