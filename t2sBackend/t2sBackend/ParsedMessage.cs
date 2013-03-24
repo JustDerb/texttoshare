311501a72ff4310c9a6c91bd60d4e52f22df2a76
Justin Derby <justderb@gmail.com>
2013-03-24 15:16:28 -0500

    Added constructor to ParsedMessage and fixed it up a bit

--- t2sBackend/t2sBackend/ParsedMessage.cs
+++ t2sBackend/t2sBackend/ParsedMessage.cs
@@ -10,65 +10,55 @@ namespace t2sBackend
     {
         
         /// <summary>
-        /// the UserDA) which sent the message
+        /// The UserDAO which sent the message
         /// </summary>
         public UserDAO Sender
         {
             get;
-  
             set;
-
-
-
         }
 
 
         /// <summary>
-        /// contains the group the message is for
+        /// Contains the group the message is for
         /// </summary>
         public GroupDAO Group
         {
             get;
-            
             set;
-            
         }
 
 
         /// <summary>
-        /// contains arg for command, each arg is a entry in list. will be space delimited when arrives from a Message
+        /// Contains arg for command, each arg is a entry in list. will be space delimited when arrives from a Message
         /// </summary>
         public List<string> Message
         {
-            /// <summary>
-            /// getter for the List of string called Message
-            /// </summary>
             get;
-
-
             set;
-           
         }
 
         /// <summary>
-        /// entire message unchanged from user expect for group id having been removed
+        /// Entire message unchanged from user expect for group id having been removed
         /// </summary>
         public string ContentMessage
         {
             get;
-            
             set;
-            
         }
 
         /// <summary>
-        /// the command that is passed from a Message. Will be space delimited in Message
+        /// The command that is passed from a Message. Will be space delimited in Message
         /// </summary>
         public string PluginMessage
         {
             get;
-
             set;
+        }
+
+        public ParsedMessage()
+        {
+            this.Message = new List<string>();
         }
     }
 }
