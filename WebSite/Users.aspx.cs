7c25c09159952dfea73d7654122ee8de60493906
staley7 <staleylance@gmail.com>
2013-03-25 12:03:49 -0500

    website working on inserting and pulling from database

---
+++ WebSite/Users.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using DataSetTableAdapters;
+public partial class Users : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        usersTableAdapter usersAdapter = new
+         usersTableAdapter();
+        DateTime time = new DateTime(2013, 3, 2);
+        usersAdapter.Insert(1, "lance", "staley", "641-373-2356", "641-373-235@email.uscc.net", 1, 2, false, false, time.Date);
+
+
+
+        GridView1.DataSource = usersAdapter.GetUsers();
+        GridView1.DataBind();
+    }
+}
