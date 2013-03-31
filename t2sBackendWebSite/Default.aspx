de65ac5f3f47e2386b840ad7bc567a3339bf272a
staley7 <staleylance@gmail.com>
2013-03-30 21:39:42 -0500

    working on creating a user in the website

---
+++ t2sBackendWebSite/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Inherits="Codebehind.Default" src="Default.cs" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title> Register User</title>
+</head>
+<body>
+   <form id="RegisterForm" runat="server">
+        <asp:textbox id="firstNameBox" text="First Name" runat="server"></asp:textbox>
+        <asp:textbox id="lastNameBox" text="Last Name" runat="server"></asp:textbox>
+        <asp:textbox id="userNameBox" text="userName" runat="server"></asp:textbox>
+        <asp:textbox id="PhoneNumberBox" text="Phone Number" runat="server"></asp:textbox>
+        <asp:textbox id="CarrierBox" text="Phone Carrier" runat="server"></asp:textbox>
+        <asp:textbox id="passwordBox" text="Password" runat="server"></asp:textbox>
+        <asp:button id="register" text="Register User" Onclick="Register_Click" runat="server"></asp:button>
+        <!--<asp:label id="FirstLabel" runat="server" /> -->
+    <div>
+    
+    </div>
+    </form>
+    <form>
+        <asp:textbox id="rFirstNamebox" text="First Name" runat="server"></asp:textbox>
+        <asp:textbox id="rLastNameBox" text="Last Name" runat="server"></asp:textbox>
+        <asp:textbox id="ruserNameBox" text="userName" runat="server"></asp:textbox>
+        <asp:textbox id="rPhoneNumber" text="Phone Number" runat="server"></asp:textbox>
+        <asp:textbox id="rCarrierBox" text="Phone Carrier" runat="server"></asp:textbox>
+        <asp:button id="getUser" text="Get User" Onclick="getRegister_Click" runat="server"></asp:button>
+
+
+    </form>
+
+
+</body>
+</html>
---
+++ t2sBackendWebSite/Default.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using t2sDbLibrary;
+
+namespace codebehind
+{
+    /// <summary>
+    /// Summary description for Default
+    /// </summary>
+    public class Default : Page
+    {
+        public Default()
+        { 
+        }
+
+
+        protected System.Web.UI.WebControls.Label MyLabel;
+        protected System.Web.UI.WebControls.Button MyButton;
+        protected System.Web.UI.WebControls.TextBox MyTextBox;
+
+        /// <summary>
+        /// registers a user 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Register_Click(Object sender, EventArgs e)
+        {
+           // MyLabel.Text = FirstTextBox.Text.ToString();
+            SqlController controller = new SqlController();
+            UserDAO user = new UserDAO();
+           //grab input from textboxs
+            String firstName = Request["firstNameBox"];
+            String lastName = Request["lastNameBox"];
+            String phoneNumber = Request["PhoneNumberBox"];
+            String phoneCarrier = Request["CarrierBox"];
+            String password = Request["passwordBox"];
+            String userName = Request["userNameBox"];
+           
+           //set fields of the userDAO
+            user.UserName = userName;
+            user.FirstName = firstName;
+            user.LastName = lastName;
+            user.PhoneNumber = phoneNumber;
+            //to be be able to retrieve from database correct carrier ending
+            user.PhoneEmail = phoneNumber + "@txt.att.com";
+            user.IsBanned = false;
+            user.IsSuppressed = false;
+            //check to see is needs to be hashed before
+            controller.CreateUser(user,password);
+        }
+
+        /// <summary>
+        /// returns the query user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void getRegister_Click(Object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+}
