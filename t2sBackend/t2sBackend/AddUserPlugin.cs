ec3fc7f42d0509593a0fc56b1fddd1edddd1d60d
Nicole Bruck <nicole.a.bruck@gmail.com>
2013-04-01 19:04:25 -0500

    Created All Default Plugin Classes

---
+++ t2sBackend/t2sBackend/AddUserPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace t2sBackend
+{
+    public class AddUserPlugin : IPlugin
+    {
+        public t2sDbLibrary.PluginDAO PluginDAO
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Run(ParsedMessage message, AWatcherService service)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
