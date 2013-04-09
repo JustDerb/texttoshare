a9cd627f6f8bad9cb971c458439c4409789d329b
Justin Derby <justderb@gmail.com>
2013-04-08 21:03:51 -0500

    First iteration of MessageParser (Still needs testing)

--- t2sBackend/t2sBackend/MessageParser.cs
+++ t2sBackend/t2sBackend/MessageParser.cs
@@ -23,41 +23,164 @@ namespace t2sBackend
         /// <param name="message"></param>
         public static ParsedMessage Parse(Message message, IDBController controller)
         {
-            String[] cmdArray = message.FullMessage.Split(delimiter);
-            String command = cmdArray[0];
-            String[] idArray = cmdArray[1].Split(secondDelimiter);
-            String groupId = idArray[0];
-            String args = "";
-
-
-
+            // Fix up the string a little
+            message.FullMessage = message.FullMessage.Trim();
 
             ParsedMessage parsed = new ParsedMessage();
-            for (int i = 1; i < idArray.Length; i++)
-            {
-                parsed.Arguments.Add(idArray[i]);
-                args = args + idArray[i];
-            }
-            parsed.Command = command;
-            parsed.ContentMessage = args;
-            try
-            {
-                parsed.Group = controller.RetrieveGroup(groupId);
-            }
-            catch (Exception)
-            {
-                parsed.Group = null;
-            }
+            TryFindSender(parsed, message, controller);
+
+            parsed.Command = FindCommand(message.FullMessage, controller);
+            parsed.ContentMessage = FindArguments(message.FullMessage, controller);
+            parsed.Arguments.AddRange(parsed.ContentMessage.Split(' '));
+
+            String groupId = FindGroup(message.FullMessage, controller);
+
+            TryFindGroup(parsed, groupId, controller);        
+            
+            return parsed;
+        }
+
+        private static void TryFindSender(ParsedMessage parsed, Message message, IDBController controller)
+        {
             try
             {
                 parsed.Sender = controller.RetrieveUserByPhoneEmail(message.Sender);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                parsed.Sender = null;
+                if (ex is ArgumentNullException ||
+                    ex is CouldNotFindException)
+                {
+                    parsed.Sender = null;
+                }
+                else
+                {
+                    // Not an exception we deal with, throw so we don't hide bugs
+                    throw;
+                }
             }
-             return parsed;
-            
+        }
+
+        private static void TryFindGroup(ParsedMessage parsed, String groupId, IDBController controller)
+        {
+            try
+            {
+                parsed.Group = controller.RetrieveGroup(groupId);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ArgumentNullException ||
+                    ex is CouldNotFindException)
+                {
+                    parsed.Group = null;
+                }
+                else
+                {
+                    // Not an exception we deal with, throw so we don't hide bugs
+                    throw;
+                }
+            }
+        }
+
+        private static String FindCommand(String message, IDBController controller)
+        {
+            // Try and find our plugin at the start
+            List<string> plugins = controller.RetrieveEnabledPluginNameList();
+            foreach (string plugin in plugins)
+            {
+                if (message.StartsWith(plugin, StringComparison.OrdinalIgnoreCase))
+                {
+                    return plugin;
+                }
+            }
+
+            // Get our bes guess by seeing if there is the first delimeter
+            int delimiter1Index = message.IndexOf(MessageParser.delimiter);
+            if (delimiter1Index < 0)
+            {
+                return "";
+            }
+            else
+            {
+                // Do we have spaces? (Plugins shouldn't have spaces)
+                string pluginMaybe = message.Substring(0, delimiter1Index).Trim();
+
+                if (pluginMaybe.Contains(' '))
+                {
+                    return "";
+                }
+                else
+                {
+                    // We found an plugin! (Hopefully)
+                    return pluginMaybe.Trim();
+                }
+            }
+        }
+
+        private static String FindGroup(String message, IDBController controller)
+        {
+            Tuple<int, int> indexes = MessageParser.FindGroupRange(message, controller);
+            if (indexes == null)
+            {
+                return "";
+            }
+            else
+            {
+                message = message.Substring(indexes.Item1, indexes.Item2).Trim();
+                return message;
+            }
+        }
+
+        private static Tuple<int, int> FindGroupRange(String message, IDBController controller)
+        {
+            // Look for our first delimiter
+            int firstDelimiterIndex = message.IndexOf(MessageParser.delimiter);
+            if (firstDelimiterIndex < 0)
+            {
+                return null;
+            }
+            firstDelimiterIndex++;
+            // Return if the delimiter is the last character
+            if (firstDelimiterIndex >= message.Length)
+            {
+                return null;
+            }
+            else
+            {
+                message = message.Substring(firstDelimiterIndex);
+                int secondDelimiterIndex = message.IndexOf(MessageParser.secondDelimiter);
+                if (secondDelimiterIndex < 0)
+                {
+                    return null;
+                }
+
+                // Grab that section
+                return new Tuple<int,int>(firstDelimiterIndex, secondDelimiterIndex);
+            }
+        }
+
+        private static String FindArguments(String message, IDBController controller)
+        {
+            // Find the arguments right after the group ID
+            Tuple<int, int> indexes = MessageParser.FindGroupRange(message, controller);
+            if (indexes == null)
+            {
+                return "";
+            }
+            else
+            {
+                // Don't include last character
+                int argStartIndex = indexes.Item2 + 1;
+                if (argStartIndex >= message.Length)
+                {
+                    return "";
+                }
+                else
+                {
+                    message = message.Substring(argStartIndex).Trim();
+                    return message;
+                }
+            }
         }
     }
 }
