73e14a57093e01cc111dd94fb4f0381a88616800
Nicole Bruck <nicole.a.bruck@gmail.com>
2013-04-10 20:23:12 -0500

    ListEnabledPlugins Now Checks for Access Level
    Handling which Plugins the Sender is able to access

--- t2sBackend/t2sBackend/DefaultPlugins/ListEnabledPlugin.cs
+++ t2sBackend/t2sBackend/DefaultPlugins/ListEnabledPlugin.cs
@@ -12,16 +12,21 @@ namespace t2sBackend
     public class ListEnabledPlugins : IPlugin
     {
         /// <summary>
-        /// Takes in the ParsedMessage and sends a message back to the sender with a message listing the enabled plugins for the group
+        /// Takes in the ParsedMessage and sends a message back to the sender with a message 
+        /// listing the enabled plugins for the group
         /// </summary>
         /// <param name="message">Message in error to be handled</param>
         /// <param name="service">Service to send/recieve messages through</param>
         public void Run(ParsedMessage message, AWatcherService service, IDBController controller)
         {
             Message msg = new Message();
-            foreach(PluginDAO d in message.Group.EnabledPlugins)
+            foreach (PluginDAO d in message.Group.EnabledPlugins)
             {
-                msg.FullMessage += d.Name + " ";
+                if (d.Access == PluginAccess.STANDARD || (d.Access == PluginAccess.MODERATOR && 
+                    (message.Group.Moderators.Contains(message.Sender) || message.Group.Owner.Equals(message.Sender))))
+                {
+                    msg.FullMessage += d.Name + " ";
+                }
             }
             msg.Reciever.Add(message.Sender.PhoneEmail);
             service.SendMessage(msg);
