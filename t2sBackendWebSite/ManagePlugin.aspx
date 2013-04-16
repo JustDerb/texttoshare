5227bdc52cebb91a3e71ecf84955d2fba5c1632a
staley7 <staleylance@gmail.com>
2013-04-15 20:47:12 -0500

    updating manage plugin pages #7

--- t2sBackendWebSite/ManagePlugin.aspx
+++ t2sBackendWebSite/ManagePlugin.aspx
@@ -7,7 +7,7 @@
     <title></title>
 </head>
 <body>
-    <form id="managePlugin" runat="server">
+    <form id="managePlugin" runat="server" >
 
         <<asp:textbox id="pluginNameBox" placeholder="Plugin Name" runat="server"></asp:textbox>
         <asp:textbox id="pluginOwner" placeholder="Owner's Users Name" runat="server"></asp:textbox>
@@ -16,7 +16,9 @@
         <asp:textbox id="versionBox" placeholder="VersionNumber" runat="server"></asp:textbox>
         <asp:button id="updatePluginButton" text="Update Plugin" Onclick="updatePlugin_Click" runat="server"></asp:button>
         <!--<asp:DropDownList-->
-
+        <asp:DropDownList id="ddlPlugin" runat="server" onload="popluateList" >
+            <asp:ListItem Text="<Select Plugin>" Value="0" />
+        </asp:DropDownList>
 
     <div>
     
--- t2sBackendWebSite/ManagePlugin.cs
+++ t2sBackendWebSite/ManagePlugin.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 using t2sDbLibrary;
 
 
@@ -24,5 +25,37 @@ namespace managePlug
 
 
         }
+
+        public void popluateList(Object sender, EventArgs e)
+        {
+
+
+            try
+            {
+
+                SqlController controller = new SqlController();
+
+               List<PluginDAO> plugins = controller.RetrieveEnabledPlugins();
+                String[] names = new String[plugins.Count];
+                ListItem[] list = new ListItem[plugins.Count];
+                for (int i = 0; i < plugins.Count; i++)
+                {
+                    ListItem item = new ListItem(plugins.ElementAt(i).Name);
+                    list[i] = item;
+                    // names[i] = plugins.ElementAt(i).Name;
+                }
+                // DropDownList menu = new DropDownList();
+                DropDownList ddlPlugin = ((DropDownList)((DropDownList)sender).Parent.FindControl("ddlPlugin"));
+
+                ddlPlugin.Items.AddRange(list);
+                // menu.Items.AddRange(list);
+
+            }
+            catch (SqlException error)
+            {
+                // Logger.LogMessage("SQL exception with Retrieve Enabled plugin");
+            }
+        }
+
     }
 }
