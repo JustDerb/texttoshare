9a5ff97760d6e571b666d58a8e34e18cc55dc547
staley7 <staleylance@gmail.com>
2013-04-15 21:40:27 -0500

    updated manage group page

--- t2sBackendWebSite/ManageGroup.aspx
+++ t2sBackendWebSite/ManageGroup.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true"Inherits="manageGroup.ManageGroup" src="ManageGroup.cs" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" Inherits="manageGroup.ManageGroup" src="ManageGroup.cs" %>
 
 <!DOCTYPE html>
 
@@ -9,20 +9,15 @@
 <body>
     <form id="updateform" runat="server">
         <asp:textbox id="groupNameBox" placeholder="Group Name" runat="server"></asp:textbox>
-         <asp:textbox id="groupTagBox" placeholder="Group Tag" runat="server"></asp:textbox>
-         <asp:textbox id="groupDescripationBox" placeholder="Descripation of Group" runat="server"></asp:textbox>
+        <asp:textbox id="groupTagBox" placeholder="Group Tag" runat="server"></asp:textbox>
+        <asp:textbox id="groupDescripationBox" placeholder="Descripation of Group" runat="server"></asp:textbox>
+        <asp:DropDownList id="groupPlugin" runat="server" onload="popluateGroupList" >
+            <asp:ListItem Text="<Select Plugin>" Value="0" />
+        </asp:DropDownList>
+        <asp:DropDownList id="enabledPlugins" runat="server" onload="retrieveEnabledGroupPlugins" >
+            <asp:ListItem Text="<Curretnly Enabled Plugins>" Value="0" />
+        </asp:DropDownList>
         <asp:button id="updateGroupButton" text="Update Group" Onclick="updateGroup_Click" runat="server"></asp:button>
-
-         <asp:CheckBoxList 
-            ID="ComboBox1"
-            DataSourceID="" 
-            DataTextField="Title" 
-            DataValueField="Id" 
-            MaxLength="0" 
-            runat="server" >
-        </asp:CheckBoxList>
-
-
     <div>
     
     </div>
--- t2sBackendWebSite/ManageGroup.cs
+++ t2sBackendWebSite/ManageGroup.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 using t2sDbLibrary;
-
 
 
 namespace manageGroup
@@ -33,21 +33,83 @@ namespace manageGroup
             String groupName = Request["groupNameBox"];
             String groupTag = Request["groupTagBox"];
             String groupDescription = Request["groupDescripationBox"];
-            group = controller.RetrieveGroup(groupTag);
-            group.Name = groupName;
-            group.GroupTag = groupTag;
-            group.Description = groupDescription;
+            try
+            {
+                group = controller.RetrieveGroup(groupTag);
+                group.Name = groupName;
+                group.GroupTag = groupTag;
+                group.Description = groupDescription;
+                //update the group
+                try
+                {
+                    controller.UpdateGroup(group);
+                }
+                catch (ArgumentNullException error)
+                {
 
-        }
+                }
+            }
+            catch (ArgumentNullException error)
+            {
 
+            }
+            catch (CouldNotFindException error)
+            {
 
-        public void createListBox()
-        {
-            SqlController controller = new SqlController();
-            List<UserDAO> users = controller.GetAllUsers();
-            ListBox allUser = new ListBox();
+            }
+           
+           
             
+            
+
         }
+
+
+        public void popluateGroupList(Object sender, EventArgs e)
+        {
+            try
+            {
+                SqlController controller = new SqlController();
+                List<PluginDAO> plugins = controller.RetrieveEnabledPlugins();
+                String[] names = new String[plugins.Count];
+                ListItem[] list = new ListItem[plugins.Count];
+                for (int i = 0; i < plugins.Count; i++)
+                {
+                    ListItem item = new ListItem(plugins.ElementAt(i).Name);
+                    list[i] = item;
+                }
+                DropDownList groupPlugin = ((DropDownList)((DropDownList)sender).Parent.FindControl("groupPlugin"));
+                groupPlugin.Items.AddRange(list);
+            }
+            catch (SqlException error)
+            {
+                // Logger.LogMessage("SQL exception with Retrieve Enabled plugin");
+            }
+        }
+
+        public void retrieveEnabledGroupPlugins(Object sender, EventArgs e)
+        {
+            try
+            {
+                SqlController controller = new SqlController();
+                GroupDAO group = controller.RetrieveGroup(Request["groupTagBox"]);
+                List<PluginDAO> plugins = controller.GetAllEnabledGroupPlugins(group.GroupID);
+                String[] names = new String[plugins.Count];
+                ListItem[] list = new ListItem[plugins.Count];
+                for (int i = 0; i < plugins.Count; i++)
+                {
+                    ListItem item = new ListItem(plugins.ElementAt(i).Name);
+                    list[i] = item;
+                }
+                DropDownList enabledPlugins = ((DropDownList)((DropDownList)sender).Parent.FindControl("enabledPlugins"));
+                enabledPlugins.Items.AddRange(list);
+            }
+            catch (SqlException error)
+            {
+                // Logger.LogMessage("SQL exception with Retrieve Enabled plugin");
+            }
+        }
+
 
 
     }
