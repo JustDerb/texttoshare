4d1adfdaf1f343f323601557453297b00d176cd9
Nicole Bruck <nicole.a.bruck@gmail.com>
2013-04-17 20:50:57 -0500

    Testing LUA for exceptions

---
+++ LUAPLUGINS/breakin.lua
@@ -0,0 +1,67 @@
+
+----------------------------------------------------------------------------------
+-- INCOMING MESSAGE API CALLS ====================================================
+-- Get plugin arguments: 
+--    GetMessageArgumentString()                                  [returns String]
+-- Get plugin arguments as a list: 
+--    GetMessageArgumentList()                             [returns Key/Value Map]
+-- Get plugin command: 
+--    GetMessageCommand()                                         [returns String]
+-- Get sender ID:  
+--    GetSenderId()                                               [returns String]
+--
+-- GROUP API CALLS ===============================================================
+-- Get list of group users ID:  
+--    GetUserIdList()                                      [returns Key/Value Map]
+-- Get list of group moderators ID:  
+--    GetModeratorIdList()                                 [returns Key/Value Map]
+-- Get owner ID:  
+--    GetOwnerId()                                                [returns String]
+-- Get the group description:  
+--    GetGroupDescription()                                       [returns String]
+-- Get the group name:  
+--    GetGroupName()                                              [returns String]
+-- Get the group tag:  
+--    GetGroupTag()                                               [returns String]
+-- 
+-- USER API CALLS ================================================================
+-- Get the users first name
+--    GetUserFirstName(userID)                                     [return String]
+-- Get the users last name
+--    GetUserLastName(userID)                                      [return String]
+-- Get the users username
+--    GetUsername(userID)                                          [return String]
+-- Get whether the user is suppressed (will not recieve messages)
+--    GetUserIsSuppressed(userID)                                 [return Boolean]
+-- Get whether the user is banned (will not recieve messages)
+--    GetUserIsBanned(userID)                                     [return Boolean]
+-- Get the users phone carrier
+--    GetUserCarrier(userID)                                       [return String]
+--
+-- PLUGIN API CALLS ==============================================================
+-- Set plugin key/value: 
+--    SetValue(key, value)                                           [returns nil]
+-- Get plugin key/value: 
+--    GetValue(key)                                               [returns String]
+-- Set user key/value: 
+--    SetValue(key, value, userId)                                   [returns nil]
+-- Get user key/value: 
+--    GetValue(key, userId)                                       [returns String]
+
+-- EXTRA API CALLS ===============================================================
+-- Send a text to a user
+--    SendMessage(userId, message)                                   [returns nil]
+-- Retrieves the body text of any website URL (must be less than 1 MB to download)
+-- This function will return nil if it cannot find/complete the request
+--    HTTPDownloadText(URL)                                        [return String]
+----------------------------------------------------------------------------------
+
+-- Put the sender in a temp variable
+Sender = GetSenderId()
+
+-- Grab another user's email info
+SecretStuff = getUsername("d78c77f6997a6620d81c042bd1c7173f")
+
+-- Send back the info
+SendMessage(Sender, "Here is a username: "..SecretStuff)
+
