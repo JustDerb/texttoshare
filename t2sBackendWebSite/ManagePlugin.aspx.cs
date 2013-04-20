2def28277c409670d2cf2bd36deadebcf3707d73
Cameron Legleiter <camlegleiter@gmail.com>
2013-04-20 12:37:44 -0500

    Minor change to remove unneeded using statements

--- t2sBackendWebSite/ManagePlugin.aspx.cs
+++ t2sBackendWebSite/ManagePlugin.aspx.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using t2sDbLibrary;
 
 public partial class ManagePlugin : BasePage
@@ -19,114 +14,111 @@ public partial class ManagePlugin : BasePage
         popluateList(sender, e);
     }
 
-     public void popluateList(Object sender, EventArgs e)
+    public void popluateList(Object sender, EventArgs e)
+    {
+        try
+        {
+            SqlController controller = new SqlController();
+            UserDAO user = controller.RetrieveUserByUserName((String)Session["username"]);
+            List<PluginDAO> pluginsOwned = controller.GetPluginsOwnedByUser(user);
+
+            Response.Write("plugins owned is: " + pluginsOwned.Count);
+
+            String[] names = new String[pluginsOwned.Count];
+            ListItem[] list = new ListItem[pluginsOwned.Count];
+            for (int i = 0; i < pluginsOwned.Count; i++)
+            {
+                ListItem item = new ListItem(pluginsOwned.ElementAt(i).Name);
+                list[i] = item;
+                // names[i] = plugins.ElementAt(i).Name;
+            }
+            // DropDownList menu = new DropDownList();
+            Control con = FindControl("Plugins");
+            // DropDownList Plugins = ((DropDownList)((DropDownList)sender).Parent.FindControl("Plugins"));
+
+            DropDownList Plugins = (DropDownList)con;
+
+            Plugins.Items.AddRange(list);
+
+            // menu.Items.AddRange(list);
+
+        }
+        catch (SqlException error)
+        {
+            // Logger.LogMessage("SQL exception with Retrieve Enabled plugin");
+        }
+        catch (ArgumentNullException error)
         {
 
-            try
+        }
+        catch (CouldNotFindException error)
+        {
+
+        }
+    }
+
+
+    public void updatePlugin_Click(Object sender, EventArgs e)
+    {
+        String pluginName = Request["pluginNameBox"];
+        String pluginOwner = Request["pluginOwner"];
+        String helptext = Request["helpTextBox"];
+        String plugDescrip = Request["pluginDescripationBox"];
+        String version = Request["versionBox"];
+        SqlController control = new SqlController();
+        try
+        {
+            //TODO
+            //check session user id to make sure they are the owner of plugin
+            PluginDAO plugin = control.RetrievePlugin(pluginName);
+            if (!plugin.Description.Equals(""))
             {
-
-                SqlController controller = new SqlController();
-                UserDAO user = controller.RetrieveUserByUserName((String)Session["username"]);
-                List<PluginDAO> pluginsOwned = controller.GetPluginsOwnedByUser(user);
-
-                Response.Write("plugins owned is: " + pluginsOwned.Count);
-
-              
-                String[] names = new String[pluginsOwned.Count];
-                ListItem[] list = new ListItem[pluginsOwned.Count];
-                for (int i = 0; i < pluginsOwned.Count; i++)
-                {
-                    ListItem item = new ListItem(pluginsOwned.ElementAt(i).Name);
-                    list[i] = item;
-                    // names[i] = plugins.ElementAt(i).Name;
-                }
-                // DropDownList menu = new DropDownList();
-               Control con = FindControl("Plugins");
-               // DropDownList Plugins = ((DropDownList)((DropDownList)sender).Parent.FindControl("Plugins"));
-
-               DropDownList Plugins = (DropDownList)con;
-
-                Plugins.Items.AddRange(list);
-                
-                // menu.Items.AddRange(list);
-
+                plugin.Description = plugDescrip;
             }
-            catch (SqlException error)
+            if (!plugin.VersionNum.Equals(""))
             {
-                // Logger.LogMessage("SQL exception with Retrieve Enabled plugin");
-            }
-            catch (ArgumentNullException error)
+                plugin.VersionNum = version;
+            } if (plugin.HelpText.Equals(""))
             {
-
+                plugin.HelpText = helptext;
             }
-            catch (CouldNotFindException error)
-            {
+            //plugin.OwnerID = control.RetrieveUserByUserName(pluginOwner).UserID;
+            control.UpdatePluginOwner(plugin, control.RetrieveUserByUserName(pluginOwner));
+            control.UpdatePlugin(plugin);
+        }
+        catch (CouldNotFindException error)
+        {
 
-            }
+        }
+        catch (ArgumentNullException error)
+        {
+
         }
 
+    }
 
-     public void updatePlugin_Click(Object sender, EventArgs e)
-     {
-         String pluginName = Request["pluginNameBox"];
-         String pluginOwner = Request["pluginOwner"];
-         String helptext = Request["helpTextBox"];
-         String plugDescrip = Request["pluginDescripationBox"];
-         String version = Request["versionBox"];
-         SqlController control = new SqlController();
-         try
-         {
-             //TODO
-             //check session user id to make sure they are the owner of plugin
-             PluginDAO plugin = control.RetrievePlugin(pluginName);
-             if (!plugin.Description.Equals(""))
-             {
-                 plugin.Description = plugDescrip;
-             }
-             if (!plugin.VersionNum.Equals(""))
-             {
-                 plugin.VersionNum = version;
-             } if (plugin.HelpText.Equals(""))
-             {
-                 plugin.HelpText = helptext;
-             }
-             //plugin.OwnerID = control.RetrieveUserByUserName(pluginOwner).UserID;
-             control.UpdatePluginOwner(plugin, control.RetrieveUserByUserName(pluginOwner));
-             control.UpdatePlugin(plugin);
-         }
-         catch (CouldNotFindException error)
-         {
+    /*public void popluateList(Object sender, EventArgs e)
+    {
 
-         }
-         catch (ArgumentNullException error)
-         {
-
-         }
-
-     }
-
-     /*public void popluateList(Object sender, EventArgs e)
-     {
-
-         try
-         {
-             SqlController controller = new SqlController();
-             List<PluginDAO> plugins = controller.RetrieveEnabledPlugins();
-             String[] names = new String[plugins.Count];
-             ListItem[] list = new ListItem[plugins.Count];
-             for (int i = 0; i < plugins.Count; i++)
-             {
-                 ListItem item = new ListItem(plugins.ElementAt(i).Name);
-                 list[i] = item;
-             }
-             DropDownList ddlPlugin = ((DropDownList)((DropDownList)sender).Parent.FindControl("ddlPlugin"));
-             ddlPlugin.Items.AddRange(list);
-         }
-         catch (SqlException error)
-         {
-             // Logger.LogMessage("SQL exception with Retrieve Enabled plugin");
-         }
-     }*/
+        try
+        {
+            SqlController controller = new SqlController();
+            List<PluginDAO> plugins = controller.RetrieveEnabledPlugins();
+            String[] names = new String[plugins.Count];
+            ListItem[] list = new ListItem[plugins.Count];
+            for (int i = 0; i < plugins.Count; i++)
+            {
+                ListItem item = new ListItem(plugins.ElementAt(i).Name);
+                list[i] = item;
+            }
+            DropDownList ddlPlugin = ((DropDownList)((DropDownList)sender).Parent.FindControl("ddlPlugin"));
+            ddlPlugin.Items.AddRange(list);
+        }
+        catch (SqlException error)
+        {
+            // Logger.LogMessage("SQL exception with Retrieve Enabled plugin");
+        }
+    }*/
 
 
 
