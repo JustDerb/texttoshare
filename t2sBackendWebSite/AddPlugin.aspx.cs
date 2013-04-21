97ec4660d40b90cfca88606704e5d50a9cff17ed
Cameron Legleiter <camlegleiter@gmail.com>
2013-04-21 12:56:16 -0500

    Minor spelling correction to correctly grab plugin description when adding. #38.

--- t2sBackendWebSite/AddPlugin.aspx.cs
+++ t2sBackendWebSite/AddPlugin.aspx.cs
@@ -22,7 +22,7 @@ public partial class AddPlugin : BasePage
         SqlController controller = new SqlController();
         PluginDAO plugin = new PluginDAO();
         UserDAO owner = new UserDAO();
-        plugin.Description = Request["pluginDescripationBox"];
+        plugin.Description = Request["pluginDescriptionBox"];
         plugin.HelpText = Request["helpTextBox"];
         plugin.IsDisabled = false;
         plugin.VersionNum = Request["versionBox"];
