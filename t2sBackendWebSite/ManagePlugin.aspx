eaa1766d49d7ac6822ddbb1ea1e7cb92e9ef9142
Justin Derby <justderb@gmail.com>
2013-04-23 05:22:33 -0500

    #50 - ManagePlugin form validation done and working.

--- t2sBackendWebSite/ManagePlugin.aspx
+++ t2sBackendWebSite/ManagePlugin.aspx
@@ -2,87 +2,113 @@
 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head runat="server">
-        <title><asp:Literal ID="PageTitle" runat="server"></asp:Literal></title>
-        <link href="Content/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="Content/style.css" rel="stylesheet" type="text/css" />
-        <style type="text/css">
-            body {
-				padding-top: 60px;
-				padding-bottom: 40px;
-			}
+<head runat="server">
+    <title>
+        <asp:Literal ID="PageTitle" runat="server"></asp:Literal></title>
+    <link href="Content/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="Content/style.css" rel="stylesheet" type="text/css" />
+    <style type="text/css">
+        body {
+            padding-top: 60px;
+            padding-bottom: 40px;
+        }
 
-            @media (max-width: 980px) {
-                .navbar-text .pull-right {
-                    float: none;
-                    padding-left: 5px;
-                    padding-right: 5px;
-                }
+        @media (max-width: 980px) {
+            .navbar-text .pull-right {
+                float: none;
+                padding-left: 5px;
+                padding-right: 5px;
             }
-        </style>
-        <link href="Content/bootstrap/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
+        }
+    </style>
+    <link href="Content/bootstrap/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
 
-        <div class="navbar navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container-fluid">
-                    <button type="button" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="brand" href="Index.aspx">Text2Share</a>
-                    <div class="nav-collapse collapse">
-                        <ul class="nav pull-right" role="navigation">
-                            <li>
-                                <a href="ManageUser.aspx">Settings</a>
-                            </li>
-                            <li class="divider-vertical"></li>
-                            <li>
-                                <asp:HyperLink id="logoutButton" runat="server" href="Logout.aspx">Logout</asp:HyperLink>
-                            </li>
-                        </ul>
-                    </div>
+    <div class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container-fluid">
+                <button type="button" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="brand" href="Index.aspx">Text2Share</a>
+                <div class="nav-collapse collapse">
+                    <ul class="nav pull-right" role="navigation">
+                        <li>
+                            <a href="ManageUser.aspx">Settings</a>
+                        </li>
+                        <li class="divider-vertical"></li>
+                        <li>
+                            <asp:HyperLink ID="logoutButton" runat="server" href="Logout.aspx">Logout</asp:HyperLink>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
-
-        <form id="managePlugin" class="tts-form" runat="server">
-            <h2 class="tts-form-heading">Login</h2>
-            <div class="control-group">
-                <label class="control-label" for="pluginNameBox">Plugin Name</label>
-                <div class="controls">
-                    <asp:TextBox ID="pluginNameBox" class="input-block-level" name="pluginNameBox" placeholder="Plugin Name" runat="server"></asp:TextBox>
+    </div>
+    <div class="container">
+        <% if (showErrorMessage)
+            { %>
+        <div class="row-fluid">
+            <div class="span12">
+                <div class="alert alert-error">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>Error!</strong> <asp:Literal ID="errorMessage" runat="server"></asp:Literal>
                 </div>
             </div>
-            <div class="control-group">
-                <label class="control-label" for="helpTextBox">Help Text</label>
-                    <div class="controls">                
-                    <asp:TextBox ID="helpTextBox" class="input-block-level" name="helpTextBox" placeholder="Help Text" runat="server"></asp:TextBox>
+        </div>
+        <% } %>
+        <div class="row-fluid">
+            <div class="span6">
+                <form id="managePlugin" class="tts-form" runat="server">
+                    <h2 class="tts-form-heading">Plugin <a href="<%= PluginEditURL %>" id="editPluginButton" class="btn btn-info pull-right">Edit Code</a></h2>
+                    <div class="control-group">
+                        <label class="control-label" for="pluginNameBox">Plugin Name</label>
+                        <div class="controls">
+                            <asp:TextBox ID="pluginNameBox" class="input-block-level uneditable-input" name="pluginNameBox" placeholder="Plugin Name" runat="server" disabled="disabled"></asp:TextBox>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="helpTextBox">Help Text</label>
+                        <div class="controls">
+                            <asp:TextBox ID="helpTextBox" class="input-block-level" name="helpTextBox" placeholder="Help Text" runat="server"></asp:TextBox>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="pluginDescriptionBox">Plugin Description</label>
+                        <div class="controls">
+                            <asp:TextBox ID="pluginDescriptionBox" class="input-block-level" name="pluginDescriptionBox" placeholder="Description of Plugin" runat="server"></asp:TextBox>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="versionBox">Plugin Version</label>
+                        <div class="controls">
+                            <asp:TextBox ID="versionBox" class="input-block-level" name="versionBox" placeholder="(1.0.0, etc)" runat="server"></asp:TextBox>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="controls">
+                            <asp:Button type="submit" ID="updatePluginButton" class="btn btn-primary" name="updatePluginButton" Text="Update Plugin" OnClick="updatePlugin_Click" runat="server"></asp:Button>
+                            <asp:Button ID="deletePluginButton" class="btn btn-danger pull-right" name="deletePluginButton" Text="Delete" OnClick="deletePlugin_Click" runat="server"></asp:Button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="span6">
+                <div class="well">
+                    <h3>Plugin Details:</h3>
+                    <ul>
+                        <li>Plugin errors: <asp:Literal ID="PluginErrorCount" runat="server"></asp:Literal></li>
+                        <li>Is this plugin disabled? <strong><asp:Literal ID="PluginDisabled" runat="server"></asp:Literal></strong></li>
+                    </ul>
+                </div>
+                <div class="well">
+                    <p>If your plugin has been disabled, you will need to edit your LUA plugin and fix anything that is wrong with it.</p>
                 </div>
             </div>
-            <div class="control-group">
-                <label class="control-label" for="pluginDescriptionBox">Plugin Description</label>
-                    <div class="controls">                
-                    <asp:TextBox ID="pluginDescriptionBox" class="input-block-level" name="pluginDescriptionBox" placeholder="Description of Plugin" runat="server"></asp:TextBox>
-                </div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="versionBox">Plugin Version</label>
-                    <div class="controls">                
-                    <asp:TextBox ID="versionBox" class="input-block-level" name="versionBox" placeholder="(1.0.0, etc)" runat="server"></asp:TextBox>
-                </div>
-            </div>
-            <div class="control-group">
-                <div class="controls">
-                    <asp:Button ID="updatePluginButton" class="btn btn-primary" name="updatePluginButton" Text="Update Plugin" OnClick="updatePlugin_Click" runat="server"></asp:Button>
-                    <asp:Literal ID="editPluginButton" runat="server"></asp:Literal>
-                </div>
-            </div>
-            <div class="control-group">
-                <asp:Label id="invalidPlugin" class="control-label text-error" runat="server"></asp:Label>
-            </div>
-        </form>
-    </body>
+        </div>
+    </div>
+</body>
 </html>
--- t2sBackendWebSite/ManagePlugin.aspx.cs
+++ t2sBackendWebSite/ManagePlugin.aspx.cs
@@ -9,20 +9,52 @@ using t2sDbLibrary;
 
 public partial class ManagePlugin : BasePage
 {
-    private UserDAO _currentUser;
-    private PluginDAO _currentPlugin;
+    private UserDAO _currentUser = null;
+    private PluginDAO _currentPlugin = null;
+
+    public string PluginEditURL = "";
+
+    public bool showErrorMessage = false;
 
     protected void Page_Load(object sender, EventArgs e)
     {
         base.CheckLoginSession();
         _currentUser = Session["userDAO"] as UserDAO;
-
-        PageTitle.Text = "Text2Share - Manage Plugin";
-
         GetPagePlugin();
-        editPluginButton.Text = string.Format(@"<a href=""EditPlugin.aspx?pluginname={0}"" id=""editPluginButton"" class=""btn pull-right"">Edit Plugin Code</a>", HttpUtility.HtmlEncode(HttpUtility.UrlEncode(_currentPlugin.Name)));
 
-        popluateList(sender, e);
+        if (!Page.IsPostBack)
+        {
+            PopulatePage();
+        }
+    }
+
+    private void PopulatePage()
+    {
+        if (_currentPlugin != null)
+        {
+            // Are they the owner?
+            if (_currentPlugin.OwnerID != _currentUser.UserID)
+            {
+                Response.Redirect(string.Format(@"Index.aspx?error={0}", HttpUtility.UrlEncode(@"You cannot edit plugins you do not own.")));
+                return;
+            }
+
+            PluginEditURL = string.Format(@"EditPlugin.aspx?pluginname={0}", HttpUtility.HtmlEncode(HttpUtility.UrlEncode(_currentPlugin.Name)));
+            pluginNameBox.Text = _currentPlugin.Name;
+            pluginDescriptionBox.Text = _currentPlugin.Description;
+            versionBox.Text = _currentPlugin.VersionNum;
+            helpTextBox.Text = _currentPlugin.HelpText;
+
+            PluginDisabled.Text = (_currentPlugin.IsDisabled) ? "YES" : "NO";
+            PluginErrorCount.Text = new SqlController().GetPluginFailedAttemptCount(_currentPlugin.PluginID).ToString();
+        }
+        else
+        {
+            Response.Redirect(string.Format(@"Index.aspx?error={0}", HttpUtility.UrlEncode(@"An error occurred retrieving the plugin information")));
+            return;
+        }
+
+        PageTitle.Text = "Text2Share - Manage Plugin " + _currentPlugin.Name;
     }
 
     private void GetPagePlugin()
@@ -45,106 +77,104 @@ public partial class ManagePlugin : BasePage
         catch (CouldNotFindException)
         {
             Response.Redirect(string.Format(@"Index.aspx?error={0}", HttpUtility.UrlEncode(@"An unknown error occurred loading plugin data. Please try again soon.")));
-            return;
         }
         catch (SqlException ex)
         {
             Logger.LogMessage("ManagePlugin: " + ex.Message, LoggerLevel.SEVERE);
             Response.Redirect(string.Format(@"Index.aspx?error={0}", HttpUtility.UrlEncode(@"An unknown error occurred loading plugin data. Please try again soon.")));
-            return;
         }
     }
 
     private void ShowError(string error)
     {
-        invalidPlugin.Text = error;
+        showErrorMessage = true;
+        errorMessage.Text = error;
     }
 
-    public void popluateList(Object sender, EventArgs e)
+    public void deletePlugin_Click(Object sender, EventArgs e)
     {
-        try
+        if (_currentPlugin != null)
         {
-            SqlController controller = new SqlController();
-            UserDAO user = controller.RetrieveUserByUserName((String)Session["username"]);
-            List<PluginDAO> pluginsOwned = controller.GetPluginsOwnedByUser(user);
-
-            Response.Write("plugins owned is: " + pluginsOwned.Count);
-
-            String[] names = new String[pluginsOwned.Count];
-            ListItem[] list = new ListItem[pluginsOwned.Count];
-            for (int i = 0; i < pluginsOwned.Count; i++)
+            // Are they the owner?
+            if (_currentPlugin.OwnerID != _currentUser.UserID)
             {
-                ListItem item = new ListItem(pluginsOwned.ElementAt(i).Name);
-                list[i] = item;
-                // names[i] = plugins.ElementAt(i).Name;
+                Response.Redirect(string.Format(@"Index.aspx?error={0}", HttpUtility.UrlEncode(@"You cannot edit plugins you do not own.")));
             }
-            // DropDownList menu = new DropDownList();
-            Control con = FindControl("Plugins");
-            // DropDownList Plugins = ((DropDownList)((DropDownList)sender).Parent.FindControl("Plugins"));
 
-            DropDownList Plugins = (DropDownList)con;
-
-            Plugins.Items.AddRange(list);
-
-            // menu.Items.AddRange(list);
-
+            try
+            {
+                IDBController database = new SqlController();
+                if (database.DeletePlugin(_currentPlugin))
+                {
+                    Response.Redirect(string.Format(@"Index.aspx?success={0}", HttpUtility.UrlEncode(@"The plugin has been deleted.")));
+                }
+                else
+                {
+                    ShowError("Failed to delete plugin.");
+                }
+            }
+            catch (CouldNotFindException)
+            {
+                // Shouldn't happen
+            }
+            catch (ArgumentNullException)
+            {
+                // Shouldn't happen
+            }
+            catch (SqlException ex)
+            {
+                Logger.LogMessage("ManagePlugin: " + ex.Message, LoggerLevel.SEVERE);
+                ShowError("An unknown error occurred loading plugin data. Please try again soon.");
+            }
         }
-        catch (SqlException)
-        {
-            // Logger.LogMessage("SQL exception with Retrieve Enabled plugin");
-        }
-        catch (ArgumentNullException)
-        {
 
-        }
-        catch (CouldNotFindException)
-        {
-
-        }
+        PopulatePage();
     }
-
 
     public void updatePlugin_Click(Object sender, EventArgs e)
     {
-        String pluginName = Request["pluginNameBox"];
-        String pluginDescription = Request["pluginDescripationBox"];
+        //String pluginName = Request["pluginNameBox"];
+        String pluginDescription = Request["pluginDescriptionBox"];
         String pluginHelpText = Request["helpTextBox"];
         String pluginVersion = Request["versionBox"];
 
         try
         {
-            //TODO
-            //check session user id to make sure they are the owner of plugin
-            if (string.IsNullOrWhiteSpace(pluginName) || pluginName.Length >= PluginDAO.NameMaxLength)
+            // Are they the owner?
+            if (_currentPlugin.OwnerID != _currentUser.UserID)
             {
-                ShowError("Plugin name cannot be empty or all spaces, and must be less than 64 characters.");
-                return;
+                Response.Redirect(string.Format(@"Index.aspx?error={0}", HttpUtility.UrlEncode(@"You cannot edit plugins you do not own.")));
             }
+
+            //if (string.IsNullOrWhiteSpace(pluginName) || pluginName.Length >= PluginDAO.NameMaxLength)
+            //{
+            //    ShowError("Plugin name cannot be empty or all spaces, and must be less than 64 characters.");
+            //    return;
+            //}
             if (string.IsNullOrWhiteSpace(pluginDescription) || pluginDescription.Length >= PluginDAO.DescriptionMaxLength)
             {
                 ShowError("Plugin description cannot be empty or all spaces.");
-                return;
             }
-            if (string.IsNullOrWhiteSpace(pluginHelpText) || pluginHelpText.Length >= PluginDAO.HelpTextMaxLength)
+            else if (string.IsNullOrWhiteSpace(pluginHelpText) || pluginHelpText.Length >= PluginDAO.HelpTextMaxLength)
             {
                 ShowError("Plugin help text cannot be empty or all spaces, and must be less than 160 characters.");
-                return;
             }
-            if (string.IsNullOrWhiteSpace(pluginVersion) || pluginVersion.Length >= PluginDAO.VersionNumberMaxLength)
+            else if (string.IsNullOrWhiteSpace(pluginVersion) || pluginVersion.Length >= PluginDAO.VersionNumberMaxLength)
             {
                 ShowError("Plugin version number cannot be empty or all spaces, and must be less than 32 characters.");
-                return;
             }
+            else
+            {
+                // Everything checks out--set the current plugin information
+                //_currentPlugin.Name = pluginName;
+                _currentPlugin.Description = pluginDescription;
+                _currentPlugin.HelpText = pluginHelpText;
+                _currentPlugin.VersionNum = pluginVersion;
 
-            // Everything checks out--set the current plugin information
-            _currentPlugin.Name = pluginName;
-            _currentPlugin.Description = pluginDescription;
-            _currentPlugin.HelpText = pluginHelpText;
-            _currentPlugin.VersionNum = pluginVersion;
-
-            IDBController controller = new SqlController();
-            //controller.UpdatePluginOwner(_currentPlugin, _currentUser);
-            controller.UpdatePlugin(_currentPlugin);
+                IDBController controller = new SqlController();
+                //controller.UpdatePluginOwner(_currentPlugin, _currentUser);
+                controller.UpdatePlugin(_currentPlugin);
+            }
         }
         catch (CouldNotFindException)
         {
@@ -158,34 +188,8 @@ public partial class ManagePlugin : BasePage
         {
             Logger.LogMessage("ManagePlugin: " + ex.Message, LoggerLevel.SEVERE);
             ShowError("An unknown error occurred loading plugin data. Please try again soon.");
-            return;
         }
 
+        PopulatePage();
     }
-
-    /*public void popluateList(Object sender, EventArgs e)
-    {
-
-        try
-        {
-            SqlController controller = new SqlController();
-            List<PluginDAO> plugins = controller.RetrieveEnabledPlugins();
-            String[] names = new String[plugins.Count];
-            ListItem[] list = new ListItem[plugins.Count];
-            for (int i = 0; i < plugins.Count; i++)
-            {
-                ListItem item = new ListItem(plugins.ElementAt(i).Name);
-                list[i] = item;
-            }
-            DropDownList ddlPlugin = ((DropDownList)((DropDownList)sender).Parent.FindControl("ddlPlugin"));
-            ddlPlugin.Items.AddRange(list);
-        }
-        catch (SqlException error)
-        {
-            // Logger.LogMessage("SQL exception with Retrieve Enabled plugin");
-        }
-    }*/
-
-
-
 }
