f76441500422987c9ece119fc79c42b3c5da224e
Justin Derby <justderb@gmail.com>
2013-04-24 22:25:10 -0500

    Added ManagePlugins

---
+++ t2sBackendWebSite/ManagePlugins.aspx
@@ -0,0 +1,144 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ManagePlugins.aspx.cs" Inherits="ManagePlugins" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head id="Head1" runat="server">
+        <title><asp:Literal ID="PageTitle" runat="server"></asp:Literal></title>
+        <link href="Content/themes/redmond/jquery-ui-1.10.2.custom.min.css" rel="stylesheet" type="text/css">
+        <link href="Content/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="Content/style.css" rel="stylesheet" type="text/css" />
+        <style type="text/css">
+            body {
+				padding-top: 60px;
+				padding-bottom: 40px;
+			}
+
+            @media (max-width: 980px) {
+                .navbar-text .pull-right {
+                    float: none;
+                    padding-left: 5px;
+                    padding-right: 5px;
+                }
+            }
+        </style>
+        <link href="Content/bootstrap/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <button type="button" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="brand" href="Index.aspx">Text2Share</a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav pull-right" role="navigation">
+                            <li>
+                                <a href="ManageUser.aspx">Settings</a>
+                            </li>
+                            <li class="divider-vertical"></li>
+                            <li>
+                                <asp:HyperLink id="logoutButton" runat="server" href="Logout.aspx">Logout</asp:HyperLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <% if (showErrorMessage)
+               { %>
+            <div class="row-fluid">
+                <div class="span12">
+                    <div class="alert alert-error">
+                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+                        <strong>Error!</strong> <asp:Literal ID="errorMessage" runat="server"></asp:Literal>
+                    </div>
+                </div>
+            </div>
+            <% } %>
+            <% if (showSuccessMessage)
+               { %>
+            <div class="row-fluid">
+                <div class="span12">
+                    <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+                        <strong>Success!</strong> <asp:Literal ID="successMessage" runat="server"></asp:Literal>
+                    </div>
+                </div>
+            </div>
+            <% } %>
+
+            <form id="updateform" class="form-horizontal" runat="server">
+                <div class="row-fluid">
+                    <div class="span6">
+                        <h2 class="tts-form-heading">Disabled</h2>
+                        <div class="control-group">
+                            <label class="control-label" for="groupNameBox">Disabled Plugins</label>
+                            <div class="controls">
+                                <asp:TextBox id="disabledPlugins" name="disabledPlugins" TextMode="MultiLine" class="input-block-level" runat="server" ReadOnly="true"></asp:TextBox>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="span6">
+                        <h2 class="tts-form-heading">Enabled</h2>
+                        <div class="control-group">
+                            <label class="control-label" for="groupNameBox">Enabled Plugins</label>
+                            <div class="controls">
+                                <asp:TextBox id="enabledPlugins" name="enabledPlugins" TextMode="MultiLine" class="input-block-level" runat="server"></asp:TextBox>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row-fluid">
+                    <div class="span12">
+                        <asp:Button ID="submitButton" class="btn btn-primary pull-right" Text="Delete Group" OnClick="submitPluginsButton_Click" runat="server" />
+                    </div>
+                </div>
+             </form>
+        </div>
+    </body>
+
+    <script src="Scripts/jquery-1.7.1.min.js"></script>
+    <script src="Scripts/jquery-ui-1.8.20.min.js"></script>
+    <script src="Scripts/rangyinputs_jquery.min.js"></script>
+    <script src="Scripts/textinputs_jquery_src.js"></script>
+    <script src="Scripts/jquery.textarea-input-support.js"></script>
+
+    <script type="text/javascript">
+        $(function () {
+            $('#groupOwner').textareainputsupport({
+                source: function (request, response) {
+                    $.ajax({
+                        url: "Users.json.aspx",
+                        dataType: "json",
+                        data: {
+                            search: request.term
+                        },
+                        success: function (data) {
+                            response($.each(data.Users, function (item) {
+                                return {
+                                    label: item,
+                                    value: item
+                                }
+                            }));
+                        }
+                    });
+                },
+                minLength: 2,
+                open: function () {
+                    $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+                },
+                close: function () {
+                    $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+                }
+            });
+
+        });
+  </script>
+</html>
+
---
+++ t2sBackendWebSite/ManagePlugins.aspx.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using t2sDbLibrary;
+
+public partial class ManagePlugins : BasePage
+{
+    public GroupDAO _currentGroup;
+    private UserDAO _currentUser;
+
+    public bool showErrorMessage = false;
+    public bool showSuccessMessage = false;
+
+    /// <summary>
+    /// functions which are run on page load.
+    /// It checks that the user is login and sets the page title as
+    /// well as filling in the group information for the group being managed
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        base.CheckLoginSession();
+        _currentUser = Session["userDAO"] as UserDAO;
+
+        PageTitle.Text = "Text2Share - Manage Group Plugins";
+
+        string errorMessageHTML = Request.QueryString["error"];
+        if (errorMessageHTML != null)
+        {
+            showErrorMessage = true;
+            errorMessage.Text = errorMessageHTML;
+        }
+
+        string successMessageHTML = Request.QueryString["success"];
+        if (successMessageHTML != null)
+        {
+            showSuccessMessage = true;
+            successMessage.Text = successMessageHTML;
+        }
+
+        GetGroupData();
+
+        if (!Page.IsPostBack)
+        {
+            SetGroupData();
+        }
+    }
+
+    private void GetGroupData()
+    {
+        try
+        {
+            IDBController controller = new SqlController();
+            _currentGroup = controller.RetrieveGroup(Request["grouptag"]);
+        }
+        catch (ArgumentNullException)
+        {
+            // Shouldn't happen
+        }
+        catch (CouldNotFindException)
+        {
+            Response.Redirect(string.Format(@"Index.aspx?error={0}", HttpUtility.UrlEncode(@"An unknown error occurred. Please try again soon.")));
+            return;
+        }
+        catch (SqlException ex)
+        {
+            Logger.LogMessage("ManageGroup.aspx.cs: " + ex.Message, LoggerLevel.SEVERE);
+            Response.Redirect(string.Format("ManagePlugins.aspx?error={0}", HttpUtility.UrlEncode("An unknown error occurred. Please try again soon.")));
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Uses the grouptag GET parameter and retrieves the group metadata.
+    /// Populates the "Group Information" section as well.
+    /// </summary>
+    /// <exception cref="ArgumentNullException">If the given string is null.</exception>
+    /// <exception cref="CouldNotFindException">If the user for the given username could not be found.</exception>
+    /// <exception cref="SQL exception">For an unknown SQL error.</exception>
+    private void SetGroupData()
+    {
+        SqlController controller = new SqlController();
+        List<PluginDAO> DisabledPlugins = controller.GetAllDisabledGroupPlugins(_currentGroup.GroupID);
+        List<PluginDAO> EnabledPlugins = controller.GetAllEnabledGroupPlugins(_currentGroup.GroupID);
+
+        bool first = true;
+        foreach (PluginDAO plug in DisabledPlugins)
+        {
+            if (!first)
+                disabledPlugins.Text += ", ";
+            disabledPlugins.Text += plug.Name;
+
+            first = false;
+        }
+
+        first = true;
+        foreach (PluginDAO plug in EnabledPlugins)
+        {
+            if (!first)
+                enabledPlugins.Text += ", ";
+            enabledPlugins.Text += plug.Name;
+
+            first = false;
+        }
+    }
+
+    /// <summary>
+    /// Updates the group's metadata in the database 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void submitPluginsButton_Click(object sender, EventArgs e)
+    {
+        bool isMod = false;
+        string groupTag = Request.QueryString["grouptag"];
+        SqlController control = new SqlController();
+        GroupDAO group = control.RetrieveGroup(groupTag);
+        List<GroupDAO> groupList = control.GetGroupsUserIsModeratorOf(_currentUser.UserID);
+        foreach (GroupDAO x in groupList)
+        {
+            if (x.GroupID == group.GroupID)
+            {
+                isMod = true;
+            }
+        }
+
+        if (_currentGroup.Owner.UserID != _currentUser.UserID && !isMod)
+        {
+            Response.Redirect(string.Format(@"Index.aspx?error={0}", HttpUtility.UrlEncode(@"You cannot edit plugins in groups you do not own.")));
+            return;
+        }
+
+        try
+        {
+            IDBController controller = new SqlController();
+            
+            _currentGroup.EnabledPlugins = ParseFromTextArea(enabledPlugins);
+
+            control.UpdateGroupPlugins(_currentGroup);
+            
+        }
+        catch (ArgumentNullException)
+        {
+            // Shouldn't happen
+        }
+        catch (CouldNotFindException)
+        {
+            // Shouldn't happen
+        }
+        catch (SqlException ex)
+        {
+            Logger.LogMessage("ManagePlugins.aspx: " + ex.Message, LoggerLevel.SEVERE);
+            Response.Redirect(string.Format("ManagePlugins.aspx?grouptag={0}&error={1}",
+                HttpUtility.UrlEncode(_currentGroup.GroupTag),
+                HttpUtility.UrlEncode("An error occurred connecting to the server. Please try again soon.")));
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Splits up the user names in the given TextBox input, finds them in the database and adds them to a HashSet.
+    /// </summary>
+    /// <param name="textarea"></param>
+    /// <returns></returns>
+    private HashSet<PluginDAO> ParseFromTextArea(TextBox textarea)
+    {
+        string[] pluginsSplit;
+        if (textarea.Text.IndexOf(',') < 0)
+        {
+            pluginsSplit = new string[] { textarea.Text.Trim() };
+        }
+        else
+        {
+            pluginsSplit = textarea.Text.Split(',');
+        }
+
+        HashSet<PluginDAO> plugins = new HashSet<PluginDAO>();
+
+        try
+        {
+            IDBController controller = new SqlController();
+            foreach (string plug in pluginsSplit)
+            {
+                try
+                {
+                    plugins.Add(controller.RetrievePlugin(plug));
+                }
+                catch (CouldNotFindException)
+                {
+                    Response.Redirect(string.Format("ManagePlugins.aspx?grouptag={0}&error={1}{2}{3}",
+                    HttpUtility.UrlEncode(_currentGroup.GroupTag),
+                    HttpUtility.UrlEncode("Could not find plugin '"),
+                    HttpUtility.UrlEncode(plug),
+                    HttpUtility.UrlEncode("'")));
+                    return null;
+                }
+            }
+        }
+        catch (ArgumentNullException)
+        {
+            // Shouldn't happen
+        }
+        catch (SqlException ex)
+        {
+            Logger.LogMessage("ManagePlugins.aspx: " + ex.Message, LoggerLevel.SEVERE);
+            Response.Redirect(string.Format("ManagePlugins.aspx?grouptag={0}&error={1}",
+                HttpUtility.UrlEncode(_currentGroup.GroupTag),
+                HttpUtility.UrlEncode("An error occurred connecting to the server. Please try again soon.")));
+            return null;
+        }
+
+        return plugins;
+    }
+}
