b6275f6d6ffbcea171dc40f73cf264609ad1cd24
Justin Derby <justderb@gmail.com>
2013-04-24 20:42:39 -0500

    Created page to grab username data

---
+++ t2sBackendWebSite/Users.json.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Users.json.aspx.cs" Inherits="Users_json" %>
---
+++ t2sBackendWebSite/Users.json.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using t2sDbLibrary;
+
+public partial class Users_json : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Clear();
+        Response.ContentType = "application/json; charset=utf-8";
+
+        String searchFor = Request.QueryString["search"];
+
+        StringBuilder userJson = new StringBuilder();
+        userJson.Append(@"{");
+        userJson.Append(@" ""Users"" : [ ");
+
+        try
+        {
+            if (!String.IsNullOrEmpty(searchFor))
+            {
+                SqlController controller = new SqlController();
+                List<UserDAO> users = controller.GetAllUsers();
+                foreach (UserDAO user in users)
+                {
+                    if (user.UserName.IndexOf(searchFor, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        userJson.Append(@"""");
+                        userJson.Append(user.UserName);
+                        userJson.Append(@"""");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage("Users.json.aspx: " + ex.Message, LoggerLevel.SEVERE);
+        }
+
+        userJson.Append(@" ] ");
+        userJson.Append(@"}");
+
+        Response.Write(userJson.ToString());
+        Response.End();
+    }
+}
